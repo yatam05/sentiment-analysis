59e7d3e0c66aaf836c81add4008536ee4b0cbf31
Aniketh Y <ayatam5128@gmail.com>
2023-07-09 14:48:36 -0500

    init

---
+++ scraping.py
@@ -0,0 +1,54 @@
+# Array Lists
+lst : list = []
+
+# String
+poop : str = "poop"
+
+# Dictionaries
+collection_o_poops : dict = {1:"grainy", 2:"mushy"}
+
+# Int
+number_of_poops : int = 2
+
+# Float
+pecent_of_grains : float = 0.23
+
+# Booleans
+flushed = True
+flushed = False
+flushed = flushed is False
+
+# not = !
+# && = and
+# || = or
+# == = is
+
+# Functions
+def flush():
+    return 0, 12, 12
+
+# For loop
+for i in lst:
+    print(i)
+
+# If statements
+if flushed is False:
+    print("Your mom gay")
+
+# try except
+try:
+    lol = "lol"
+    lol += 1
+except TypeError:
+    print("you stupid")
+
+# Tuples
+tupy = (1,2,3,4,5,6,7,8,9,0)
+
+# While loops
+while True:
+    print("forever loop!")
+
+
+
+
